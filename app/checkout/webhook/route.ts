6876a3fc77c3a22218c6b41a65926580548d7bb7
Gabriel Medina <gabi89.m@gmail.com>
2023-10-31 17:14:52 -0300

    WIP webhooks

--- app/checkout/webhook/route.ts
+++ app/checkout/webhook/route.ts
@@ -1,35 +1,63 @@
 import { db } from '../../../lib/drizzle'
+import { paddle as paddleSchema } from '../../../lib/schema'
 
 export async function POST(request: Request) {
-
+  //console.log('Webhook received')
   try {
-    const body = await request.json()
-    console.log('Body ', body)
+    const event = await request.json()
+    //console.log('Body ', event)
 
-    switch (body.event_type) {
+    switch (event.event_type) {
       case 'subscription.created':
-        return onSubscriptionCreated()
+        onSubscriptionCreated(event)
+        break
       case 'subscription.updated':
-        return onSubscriptionUpdated()
-      case 'tranasction.completed':
-        return onTransactionCompleted()
+        onSubscriptionUpdated(event)
+        break
+      case 'transaction.completed':
+        onTransactionCompleted(event)
+        break
     }
   } catch (e) {
-
+    return Response.json({ fail: true })
   }
 
-
-  return Response.json({ fail: true })
+  return Response.json({ success: true })
+  
 }
 
-function onSubscriptionCreated() {
+async function onSubscriptionCreated(event: any) {
   // Create a new subscription record in our database
+  console.log('Subscription created ', event)
+  if (!event.data.custom_data.userId) { throw new Error('No userId coming in subscription ' + event.data.id) }
+  const userId = event.data.custom_data.userId
+  let paddle = await db.query.paddle.findFirst({ where: (paddle, { eq }) => eq(paddle.userId, userId) })
+  if (paddle) {
+    // This should never happen
+    throw new Error('The user already has a paddle record ' + userId)
+  }
+  const nextBilledAt = new Date(event.data.next_billed_at)
+  
+  try {
+    await db.insert(paddleSchema).values({
+      userId,
+      customer_id: event.data.customer_id,
+      status: event.data.status,
+      next_billed_at: nextBilledAt
+    })
+  } catch (error) {
+    throw new Error('Error inserting paddle record ' + error)
+  }
+
+  return true;
 }
 
-function onSubscriptionUpdated() {
+async function onSubscriptionUpdated(event: any) {
   // Take care of cases where subscription is canceled, paused, etc.
+  //console.log('Subscription updated ', event)
 }
 
-function onTransactionCompleted() {
+async function onTransactionCompleted(event: any) {
   // Update user's credits for the current billing cycle
+  //console.log('Tranasction completed ', event)
 }
--- middleware.js
+++ middleware.js
@@ -2,6 +2,6 @@ export { default } from "next-auth/middleware"
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|checkout/webhook|_next/static|_next/image|favicon.ico).*)',
   ]
 }
