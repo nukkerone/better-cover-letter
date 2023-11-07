190e3b791560a0f72d68cad4fee86e4dba34feeb
Gabriel Medina <gabi89.m@gmail.com>
2023-11-07 11:24:44 -0300

    Landing fix

--- app/generate/layout.tsx
+++ app/generate/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '../globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   description: 'Generate better cover letters with the help of AI',
 }
 
-export default function Layout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
