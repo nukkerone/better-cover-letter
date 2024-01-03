feba6dbf663df364f33e5daa07ed8de2a33f350b
Gabriel Medina <gabi89.m@gmail.com>
2024-01-02 23:11:56 -0300

    Removed auth, paddle and db configuration due to simplification

--- app/(auth)/generate/page.tsx
+++ app/(auth)/generate/page.tsx
@@ -6,12 +6,12 @@ import { redirect } from "next/navigation"
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
-  const paddle = await db.query.paddle.findFirst({ where: (paddle, { eq }) => eq(paddle.userId, session!.user.id) })
-  const activePayment = paddle?.status === 'active'
+  /* const paddle = await db.query.paddle.findFirst({ where: (paddle, { eq }) => eq(paddle.userId, session!.user.id) })
+  const activePayment = paddle?.status === 'active' */
 
-  if (!activePayment) {
+  /* if (!activePayment) {
     redirect('/checkout')
-  }
+  } */
 
   return (
     <main className="flex min-h-screen flex-row items-stretch justify-between p-24 max-w-[1280px] gap-[24px] m-auto">
